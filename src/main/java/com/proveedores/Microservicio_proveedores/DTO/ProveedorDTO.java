bf9af97ab986f002f7ff7d395c7792cd904856ed
Joocr <139135806+Jacq20@users.noreply.github.com>
2026-05-28 10:12:21 -0400

    actualizacion

--- src/main/java/com/proveedores/Microservicio_proveedores/DTO/ProveedorDTO.java
+++ src/main/java/com/proveedores/Microservicio_proveedores/DTO/ProveedorDTO.java
@@ -2,6 +2,10 @@ package com.proveedores.Microservicio_proveedores.DTO;
 
 import java.math.BigDecimal;
 import java.util.List;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
